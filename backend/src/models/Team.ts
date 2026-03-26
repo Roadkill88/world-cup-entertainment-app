3f90b2dcd52fa8fd9c375e142d0635d587e28474
Roadkill88 <simonmatre@hotmail.com>
2026-03-26 21:45:51 +0100

    Create Team interface for team data

---
+++ backend/src/models/Team.ts
@@ -0,0 +1,11 @@
+export interface Team {
+    teamId: number;
+    name: string;
+    flag: string;
+    group: string;
+    wins: number;
+    draws: number;
+    losses: number;
+    goalsFor: number;
+    goalsAgainst: number;
+}
